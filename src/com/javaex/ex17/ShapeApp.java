4856f67d1a0952479be24517ab96ec225fd28088
antrophus <antrophus@yahoo.com>
2024-07-10 09:48:36 +0900

    7/9 instanceof : ex17

--- src/com/javaex/ex17/ShapeApp.java
+++ src/com/javaex/ex17/ShapeApp.java
@@ -1,29 +1,36 @@
 package com.javaex.ex17;
 
-public abstract class ShapeApp extends Shape implements Drawable	{
+public abstract class ShapeApp extends Shape implements Drawable {
 
 	public static void main(String[] args) {
-		
-	Drawable r01 = new Ractangle(50, 10, "블랙", "핑크");
-	Drawable p01 = new Point(6, 5);
-	Drawable c01 = new Circle(5, "블랙", "핑크");
-	Drawable t01 = new Triangle(1, 9, "블랙", "핑크");
-	
-	Drawable[] dArray = new Drawable[4];
-	
-	dArray[0] = r01;
-	dArray[1] = p01;
-	dArray[2] = c01;
-	dArray[3] = t01;
-	
-	for(int i = 0; i<dArray.length; i++) {
-		dArray[i].draw();
-		
-		
-	}
-	
-	
-		
+
+		Drawable r01 = new Ractangle(50, 10, "블랙", "핑크");
+		Drawable p01 = new Point(6, 5);
+		Drawable c01 = new Circle(5, "블랙", "핑크");
+		Drawable t01 = new Triangle(1, 9, "블랙", "핑크");
+
+		Drawable[] dArray = new Drawable[4];
+
+		dArray[0] = r01;
+		dArray[1] = p01;
+		dArray[2] = c01;
+		dArray[3] = t01;
+
+//		for (int i = 0; i < dArray.length; i++) {
+//			dArray[i].draw();
+//
+//		}
+//	System.out.println(((Shape) r01).area());
+//	System.out.println(((Shape) c01).area());
+//	System.out.println(((Shape) t01).area());
+
+		for (int i = 0; i < dArray.length; i++) {
+
+			if (dArray[i] instanceof Shape) {
+
+				System.out.println(((Shape) dArray[i]).area());
+			}
+		}
 
 	}
 
